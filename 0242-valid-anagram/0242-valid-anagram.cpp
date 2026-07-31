9b55ada8cc973da5aabd151c1a3ec8987387eaab
Prajwal9591 <prajwalhy9591@outlook.com>
2026-07-31 19:08:44 +0530

    Time: 2 ms (50.55%), Space: 9.8 MB (26.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> freq;
+        if(s.size() != t.size()){
+            return false;
+        }
+        for(char ch : s){
+            freq[ch]++;
+        }
+        for(char ch : t){
+            if(freq[ch] == 0){
+                return false;
+            }else{
+                freq[ch]--;
+            }
+        }
+        return true;
+    }
+};
